28edb84bd243156816c89a23d35bd9fb3256dde3
Jon Jackson <ocskier@gmail.com>
2020-02-13 20:46:11 -0500

    Add Queries and Inquirer Logic Working

--- app.ts
+++ app.ts
@@ -15,9 +15,9 @@ const menu = [
     name: 'choice',
     type: 'list',
     choices: [
-      'Get Departments', 
-      'Get Roles', 
-      'Get Employees', 
+      'Get Departments',
+      'Get Roles',
+      'Get Employees',
       'Add A New Department',
       'Add A New Role',
       'Add A New Employee',
@@ -28,7 +28,62 @@ const menu = [
       'Remove A Role',
       'Remove An Employee',
       `View A Department's Budget`,
-      'Done'],
+      'Done',
+    ],
+  },
+];
+
+const roleQues = [
+  {
+    message: 'Role: ',
+    name: 'title',
+    validate: (input: string) => {
+      if (input) return true;
+      else return 'You didnt enter a name!';
+    },
+  },
+  {
+    message: 'Salary: ',
+    name: 'salary',
+    validate: (input: string) => {
+      if (!isNaN(parseFloat(input))) return true;
+      else return 'You didnt enter a valid number!';
+    },
+  },
+  {
+    message: 'Dept ID: ',
+    name: 'deptId',
+    validate: (input: string) => {
+      if (!isNaN(parseInt(input))) return true;
+      else return 'You didnt enter a valid number!';
+    },
+  },
+];
+
+const employeeQues = [
+  {
+    message: 'Full Name: ',
+    name: 'name',
+    validate: (input: string) => {
+      if (input) return true;
+      else return 'You didnt enter a name!';
+    },
+  },
+  {
+    message: 'Role Id: ',
+    name: 'roleId',
+    validate: (input: string) => {
+      if (!isNaN(parseInt(input))) return true;
+      else return 'You didnt enter a valid number!';
+    },
+  },
+  {
+    message: 'Manager Id: ',
+    name: 'managerId',
+    validate: (input: string) => {
+      if (!isNaN(parseInt(input))) return true;
+      else return 'You didnt enter a valid number!';
+    },
   },
 ];
 
@@ -36,44 +91,59 @@ const menu = [
 const init = async () => {
   // let i = 0; Reserved for later use
   while (keepRunning) {
-  const { choice } = await ask.prompt(menu);
+    const { choice } = await ask.prompt(menu);
 
-  choice && (await getChart(choice));
+    choice && (await getChart(choice));
   }
 };
 
 const getChart = async (val: string) => {
-    let result;
-    switch (val) {
-      case 'Get Departments':
-        result = await queryDb(conn, queries.getAllDepartments());
-        console.log(result);
-        break;
-      case 'Get Roles':
-        result = await queryDb(conn, queries.getAllRoles());
-        console.log(result);
-        break;
-      case 'Get Employees':
-        result = await queryDb(conn, queries.getAllEmployees());
-        console.log(result);
-        break;
-      case 'Add A New Department':
-        const {deptName} = await ask.prompt([{
+  let result, answers;
+  switch (val) {
+    case 'Get Departments':
+      result = await queryDb(conn, queries.getAllDepts());
+      console.log(result);
+      break;
+    case 'Get Roles':
+      result = await queryDb(conn, queries.getAllRoles());
+      console.log(result);
+      break;
+    case 'Get Employees':
+      result = await queryDb(conn, queries.getAllEmployees());
+      console.log(result);
+      break;
+    case 'Add A New Department':
+      const { deptName } = await ask.prompt([
+        {
           message: 'Department Name: ',
           name: 'deptName',
-          validate: (input:string) => {
+          validate: (input: string) => {
             if (input) return true;
-            else return 'You didnt enter a name!'
-          }
-        }]);
-        result = await queryDb(conn, queries.addDept(deptName));
-        console.log(result.affectedRows);
-        break;
-      case 'Done':
-        keepRunning = false;
-        conn.end();
-        break;
-    }
+            else return 'You didnt enter a name!';
+          },
+        },
+      ]);
+      result = await queryDb(conn, queries.addDept(deptName));
+      console.log(result.affectedRows);
+      break;
+    case 'Add A New Role':
+      answers = await ask.prompt(roleQues);
+      result = await queryDb(conn, queries.addRole(answers));
+      console.log(result.affectedRows);
+      break;
+    case 'Add A New Employee':
+      answers = await ask.prompt(employeeQues);
+      answers.first = answers.name.split(' ')[0];
+      answers.last = answers.name.split(' ')[1];
+      delete answers.name;
+      result = await queryDb(conn, queries.addEmployee(answers));
+      console.log(result.affectedRows);
+      break;
+    case 'Done':
+      keepRunning = false;
+      conn.end();
+      break;
+  }
 };
 
 conn.connect((err: MysqlError) => {
--- controllers/controller.ts
+++ controllers/controller.ts
@@ -1,14 +1,14 @@
 interface IRole {
   title: string;
-  salary: number;
-  deptId: number;
+  salary: string;
+  deptId: string;
 }
 
 interface IEmployee {
   first: string;
-  last:string;
-  roleId: number;
-  managerId: number;
+  last: string;
+  roleId: string;
+  managerId: string;
 }
 
 module.exports = {
@@ -21,34 +21,32 @@ module.exports = {
   getAllRoles: () => {
     return ['SELECT * FROM roles'];
   },
-  addDept: (dept:string) => {
-    return [`INSERT INTO departments(name) VALUES('${dept}')`]
+  addDept: (dept: string) => {
+    return [`INSERT INTO departments(name) VALUES('${dept}')`];
   },
-  addRole: (roleData:IRole) => {
-    return [`INSERT INTO roles(title,salary,deptId) VALUES('${roleData.title}','${roleData.salary}','${roleData.deptId}'`]
+  addRole: (roleData: IRole) => {
+    return [
+      `INSERT INTO roles(title,salary,deptId) VALUES('${
+        roleData.title
+      }',${parseFloat(roleData.salary)},${parseInt(roleData.deptId)})`,
+    ];
   },
-  addEmployee: (empData:IEmployee) => {
-    return [`INSERT INTO employees(first,last,roleId,managerId) VALUES(${empData}`]
+  addEmployee: (empData: IEmployee) => {
+    return [
+      `INSERT INTO employees(first,last,roleId,managerId) VALUES('${
+        empData.first
+      }','${empData.last}',${parseInt(empData.roleId)},${parseInt(
+        empData.managerId
+      )})`,
+    ];
   },
-  updateEmployeeRole: () => {
-
+  updateEmployeeRole: (role: number, id: number) => {
+    return [`UPDATE employees SET (roleId=${role}) WHERE id=${id}`];
   },
-  updateEmployeeManager: () => {
-
-  },
-  viewEmployeesByManager: () => {
-
-  },
-  removeDept: () => {
-
-  },
-  removeRole: () => {
-
-  },
-  removeEmployee: () => {
-
-  },
-  viewDeptBudget: () => {
-
-  }
+  updateEmployeeManager: () => {},
+  viewEmployeesByManager: () => {},
+  removeDept: () => {},
+  removeRole: () => {},
+  removeEmployee: () => {},
+  viewDeptBudget: () => {},
 };
