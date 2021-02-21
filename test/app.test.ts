31cc7c68d2d3ea8c6875c808b33a1fb01943bdcd
Jon Jackson <ocskier@gmail.com>
2021-02-20 23:30:37 -0500

    fix: update errant test file

--- test/app.test.ts
+++ test/app.test.ts
@@ -1,6 +1,3 @@
-const Manager = require('../app');
+const app = require('../app');
 
-test('Can set office number via constructor argument', () => {
-  const testValue = 100;
-  expect(100).toBe(testValue);
-});
+test('Can start app', () => {});
